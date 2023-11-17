82697cd9017bc3830a9a9dd2714929e791944f77
seeyoungryu <140057709+seeyoungryu@users.noreply.github.com>
2023-11-17 10:03:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.13 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int solution(int n) {
+        String a = "";
+
+        while(n > 0){
+            a = (n % 3) + a;
+            n /= 3;
+        }
+        a = new StringBuilder(a).reverse().toString();
+
+
+        return Integer.parseInt(a,3);
+    }
+}
