71c27be953f84bf18157249a9f401b80d7eec206
seeyoungryu <140057709+seeyoungryu@users.noreply.github.com>
2023-11-23 08:59:36 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while (n >= a) {
+            answer += b * (n / a);
+            n = b * (n / a) + n % a;
+        }
+
+        return answer;
+    }
+}
