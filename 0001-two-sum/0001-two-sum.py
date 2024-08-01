04b1b27bd8c8c0bf123648b718ca65ab3e481613
seeyoungryu <seayeux@gmail.com>
2024-08-01 19:02:33 +0900

    Time: 34 ms (89.56%), Space: 12.5 MB (37.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
